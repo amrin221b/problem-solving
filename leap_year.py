4f06ddaae9659b8b3e88efe2e2ef15c7ea2dac24
amrin221b <amrin.ece221b@gmail.com>
2022-05-24 20:26:39 +0600

    this is a code for finding leap year

---
+++ leap_year.py
@@ -0,0 +1,8 @@
+def leap(n):
+    if n%400==0 and n%100==0:
+        print(f"{n} is a leap year")
+    elif n%4==0 and n%100!=0:
+        print(f"{n} is a leap year")
+    else:
+        print(f"{n} is not a leap year")
+leap(int(input('enter the year: ')))
